316e5f59efffcf0954dbfd862b85e15c20872aea
klei.nagode <klei.nagode@gmail.com>
2024-04-03 10:35:42 -0500

    updateds

--- Class_Work/After Feb 14/Files - Explained.py
+++ Class_Work/After Feb 14/Files - Explained.py
@@ -1,4 +1,7 @@
 
+
+import os
+
 #To create a new file just write open("filename.txt" "w")
 
 myfile = open("myfile.txt","r") # Opens the file in read mode, you can also open it in w/a mode
@@ -11,6 +14,8 @@ content = myfile.read() #Reads the contenof the file in the variable
 print(content) #Prints the content
 
 myfile.close()
+
+os.remove("myfile.txt")
 
 # myfile.readlines() Returns a list of strings, the first item is the first line and so on
 # myfile.readline() Reads one line at a time
