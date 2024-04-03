07eaf8cb7ac7fcd1e29d1b03cbc22eee36706b75
klei.nagode <klei.nagode@gmail.com>
2024-04-03 10:15:53 -0500

    addded files

---
+++ Class_Work/After Feb 14/Files - Explained.py
@@ -0,0 +1,18 @@
+
+#To create a new file just write open("filename.txt" "w")
+
+myfile = open("myfile.txt","r") # Opens the file in read mode, you can also open it in w/a mode
+                                 # Opening the file in W will rewrite the content of the file
+
+content = myfile.read() #Reads the contenof the file in the variable
+
+
+
+print(content) #Prints the content
+
+myfile.close()
+
+# myfile.readlines() Returns a list of strings, the first item is the first line and so on
+# myfile.readline() Reads one line at a time
+
+
